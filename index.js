94e03de2109561d817b4aa2cc7662e4f51d2b437
BadHenry7 <141955809+BadHenry7@users.noreply.github.com>
2025-02-15 10:10:17 -0500

    guardar cambios 5173

--- index.js
+++ index.js
@@ -6,17 +6,37 @@ const especialidadesRoutes= require ('./src/routes/especialidadesRoutes');
 const hospitalesRoutes= require ('./src/routes/hospitalesRoutes');
 const salas_Routes= require ('./src/routes/salasRoutes');
 
-
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
 
+
+
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173", // Svelte en desarrollo
+        "http://localhost:3000", // Backend en desarrollo
+        "https://0315-161-10-153-168.ngrok-free.app", // Ngrok
+        "https://b45d-161-10-153-168.ngrok-free.app", // Ngrok
+        "https://api-nodejs-buxf.onrender.com"
+      ],
+      credentials: true, // Si necesitas enviar cookies o headers con autenticación
+      methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    })
+  );
+
+
+
+
+app.use(bodyParser.json());
 //rutas
 app.use('/api/personas', personaRoutes);
 app.use('/api/hospitales', hospitalesRoutes);
 app.use('/api/especialidades', especialidadesRoutes);
 app.use('/api/salas', salas_Routes);
+
 
 
 //iniciar el servidor
