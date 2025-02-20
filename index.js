87f351649d4e958668def16110573e261842cbb6
BadHenry7 <141955809+BadHenry7@users.noreply.github.com>
2025-02-20 00:31:05 -0500

    probando server y index

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(
     })
   );
 
-
+//
 
 
 app.use(bodyParser.json());
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-
+//
 app.use(express.json());
 app.use(cors());
 
