1f50e279a199bff55f01afd17d75e5cf808f22b3
BadHenry7 <141955809+BadHenry7@users.noreply.github.com>
2025-07-25 19:12:53 -0500

    se cambia a post

--- src/routes/salasRoutes.js
+++ src/routes/salasRoutes.js
@@ -6,7 +6,7 @@ const salas_controller = require ('../controllers/salasController')
 
 router.get('/getsalas', salas_controller.getsalas)
 router.get('/getsalaById/:id_hospital', salas_controller.getsalaById)
-router.get('/getsalaByNombre/:nombre_hospital', salas_controller.getsalaByNombre)
+router.post('/getsalaByNombre/:nombre_hospital', salas_controller.getsalaByNombre)
 
 router.post('/createsalas', salas_controller.createsalas)
 router.put('/updatesalas/:id_hospital', salas_controller.updatesalas)
